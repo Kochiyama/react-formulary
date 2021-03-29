23a251feb2467225fbf6eaf39dd6cb4b47544794
Kochiyama <marcelokochiyama2210@gmail.com>
2021-03-29 16:35:07 -0400

    Create main page with title and render our formulary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grid, Container, Typography } from '@material-ui/core';
+import RegisterForm from './components/RegisterForm/RegisterForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justify='center'
+      style={{ minHeight: '100vh' }}
+    >
+      <Typography variant='h2' component='h1' align='center'>
+        Form Validation
+      </Typography>
+      <Typography
+        variant='h5'
+        component='h2'
+        align='center'
+        style={{ marginBottom: 20 }}
+      >
+        With MaterialUI, Formik and Yup!
+      </Typography>
+
+      <Container maxWidth='sm'>
+        <RegisterForm />
+      </Container>
+    </Grid>
   );
 }
 
